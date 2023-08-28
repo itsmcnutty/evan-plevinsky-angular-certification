313db61dd90be5164421a341f5897be119e6aa2c
itsmcnutty <epplevinsky@wpi.edu>
2023-08-28 14:46:38 -0400

    Removed type any from all locations and replaced with appropriate typing

--- src/app/pipes/unescape-html.pipe.ts
+++ src/app/pipes/unescape-html.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class UnescapePipe implements PipeTransform {
-  transform(value: string): any {
+  transform(value: string): string | null {
     const doc = new DOMParser().parseFromString(value, 'text/html');
     return doc.documentElement.textContent;
   }
