7f7fe8098c9ba9c8c25fbfea1763bd10cd0b56e3
itsmcnutty <epplevinsky@wpi.edu>
2023-08-28 14:42:36 -0400

    Fixed stackblitz to run app properly

--- src/app/pipes/unescape-html.pipe.ts
+++ src/app/pipes/unescape-html.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   standalone: true,
 })
 export class UnescapePipe implements PipeTransform {
-  transform(value: any, args?: any): any {
+  transform(value: string): any {
     const doc = new DOMParser().parseFromString(value, 'text/html');
     return doc.documentElement.textContent;
   }
